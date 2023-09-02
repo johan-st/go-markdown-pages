1d974b49325360bc0790a3b882a05bacff4ad6e5
johan-STYL <johan@styldesign.se>
2023-09-02 13:12:12 +0200

    remove distractions

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net/http"
 	"os"
@@ -9,32 +8,16 @@ import (
 	"github.com/charmbracelet/log"
 )
 
-type app struct {
-	errorLogger  *log.Logger
-	pagesHandler *handler
-	errFatalChan chan<- error
-}
-
 func main() {
 	// flagDev := flag.Bool("dev", false, "Run in development mode. Compiles templates on every request.")
 	// flag.Parse()
 
 	logger := log.New(os.Stderr)
+	logger.SetLevel(log.DebugLevel) //TODO: remove
 	handler := newHandler(logger.WithPrefix("handler"))
 	handler.prepareRoutes()
-	errFatalChan := make(chan error)
 
-	// fatal error handler
-	go errFatal(errFatalChan)
-
-	log.Fatal(
-		tui(
-			&app{
-				errorLogger:  logger,
-				pagesHandler: handler,
-				errFatalChan: errFatalChan,
-			}))
-
+	log.Fatal(runServer(handler))
 }
 
 func runServer(h *handler) error {
@@ -49,76 +32,5 @@ func runServer(h *handler) error {
 	if err := srv.ListenAndServe(); err != nil {
 		return fmt.Errorf("ListenAndServe: %s", err)
 	}
-	return fmt.Errorf("unexpected server shutdown...")
-}
-
-// TUI
-func tui(app *app) error {
-
-	fmt.Println("___________________")
-	fmt.Println("| markdown server |")
-	fmt.Println("|_________________|")
-	fmt.Println("(type 'h' for help)")
-	fmt.Print("enter command:")
-
-	s := bufio.NewScanner(os.Stdin)
-	for s.Scan() {
-		switch s.Text() {
-		case "h":
-			tuiHelp()
-		case "q":
-			log.Info("exiting...")
-			os.Exit(0)
-		case "s":
-			log.Info("starting server...")
-			log.Warn("TODO: implement graceful shutdown, already running handler, etc.")
-			go func() {
-				app.errFatalChan <- runServer(app.pagesHandler)
-			}()
-		case "r":
-			log.Info("restarting server...")
-			log.Warn("TODO: implement")
-
-		case "1":
-			log.Print("setting log level to Fatal")
-			app.errorLogger.SetLevel(log.FatalLevel)
-		case "2":
-			log.Print("setting log level to Error")
-			app.errorLogger.SetLevel(log.ErrorLevel)
-		case "3":
-			log.Print("setting log level to Warn")
-			app.errorLogger.SetLevel(log.WarnLevel)
-		case "4":
-			log.Print("setting log level to Info")
-			app.errorLogger.SetLevel(log.InfoLevel)
-		case "5":
-			log.Print("setting log level to Debug")
-			app.errorLogger.SetLevel(log.DebugLevel)
-
-		default:
-			fmt.Println("unknown command (type 'h' for help)")
-		}
-		fmt.Print("enter command:")
-	}
-	return nil
-}
-
-func tuiHelp() {
-	fmt.Println("--commands--")
-	fmt.Println("h: help")
-	fmt.Println("s: start server")
-	fmt.Println("r: restart server")
-	fmt.Println("q: exit")
-	fmt.Println()
-	fmt.Println("--logging level--")
-	fmt.Println("1: log level Fatal")
-	fmt.Println("2: log level Error")
-	fmt.Println("3: log level Warn")
-	fmt.Println("4: log level Info (default)")
-	fmt.Println("5: log level Debug")
-}
-
-// error handlers
-func errFatal(errChan <-chan error) {
-	log.Fatal("Fatal error", "err", <-errChan)
+	return fmt.Errorf("unexpected server shutdown")
 }
